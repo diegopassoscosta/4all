90aba49092d2b821849c925d4dceb23e161a711a
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-02 23:59:49 -0300

    Adicionado alguns exemplos de sintaxe
    Signed-off-by: Diego Passos Costa <diegopassoscosta@gmail.com>

---
+++ java/se/src/br/org/senai/exemplo/sintaxe/Array/UsoDoArray.java
@@ -0,0 +1,17 @@
+package br.org.senai.exemplo.sintaxe.Array;
+
+public class UsoDoArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Declaração da variável como array
+		int[] x = new int[2];
+		x[0] = 1;
+		x[1] = 2;
+		System.out.println(x[0]);
+		System.out.println(x[1]);
+	}
+
+}
