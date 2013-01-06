067f663a80779950e3930667ed763e653d640a14
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-05 23:19:51 -0300

    Adicionando exemplo da conta corrente
    Signed-off-by: Diego Passos Costa <diegopassoscosta@gmail.com>

---
+++ java/se/src/br/org/senai/orientado/lista02/exercicio05/contacorrente/ContaCorrente.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.senai.orientado.lista02.exercicio05.contacorrente;
+
+/**
+ *
+ * @author lab01
+ */
+public class ContaCorrente {
+    
+}
