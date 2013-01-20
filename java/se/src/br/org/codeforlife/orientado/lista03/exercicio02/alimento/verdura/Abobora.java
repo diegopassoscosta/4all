7c684c6c988acafc18b75087a27b2b5532d270ba
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-20 17:22:35 -0300

    Adicionaod exercicio 2 criaçao de classe pre-interface
    Signed-off-by: Diego Passos Costa <diegopassoscosta@gmail.com>

---
+++ java/se/src/br/org/codeforlife/orientado/lista03/exercicio02/alimento/verdura/Abobora.java
@@ -0,0 +1,19 @@
+package br.org.codeforlife.orientado.lista03.exercicio02.alimento.verdura;
+
+import br.org.codeforlife.orientado.lista03.exercicio02.alimento.Alimento;
+
+/**
+ *
+ * @author lab01
+ */
+public class Abobora extends Alimento {
+   
+    /**
+     * @param peso
+     * @param energia 
+     */
+    public Abobora(int peso, int energia) {
+        super(peso, energia);
+    }
+    
+}
---
+++ java/se/src/br/org/codeforlife/orientado/lista03/exercicio02/alimento/verdura/Beterraba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.codeforlife.orientado.lista03.exercicio02.alimento.verdura;
+
+import br.org.codeforlife.orientado.lista03.exercicio02.alimento.Alimento;
+
+/**
+ *
+ * @author lab01
+ */
+public class Beterraba extends Alimento{
+    /**
+     * 
+     * @param peso
+     * @param energia 
+     */
+    public Beterraba(int peso, int energia) {
+        super(peso,energia);
+    }
+    
+}
