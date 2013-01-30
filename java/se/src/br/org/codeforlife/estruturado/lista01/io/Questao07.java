389b6f5d3e119433a4663ddc89094e364d58bcde
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-30 10:52:04 -0300

    Modificado nomes das variaveis para o padrão

--- java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao07.java
+++ java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao07.java
@@ -14,25 +14,25 @@ public class Questao07 {
 
     public static void main(String args[]) {
         //declaração de variáveis
-    float valor_da_conta = 0;
-    float valor_da_gorgeta;
+    float valorDaConta = 0;
+    float valorDaGorgeta;
     float DEZ_PORCENTO = 0.1f;
-    float valor_total;
+    float valorTotal;
     
     //solicitação do pedido
     System.out.println("Informe o valor da conta: ");    
     Scanner leia = new Scanner(System.in);
     
-    valor_da_conta = leia.nextInt();
+    valorDaConta = leia.nextInt();
     
     //calculos
-    valor_da_gorgeta = DEZ_PORCENTO * valor_da_conta;
-    valor_total = valor_da_conta + valor_da_gorgeta;
+    valorDaGorgeta = DEZ_PORCENTO * valorDaConta;
+    valorTotal = valorDaConta + valorDaGorgeta;
     
     //demonstração do resultado
-    System.out.println("valor da conta: %f\n" + valor_da_conta);
-    System.out.println("valor da gorgeta: %f\n" + valor_da_gorgeta);
+    System.out.println("valor da conta: %f\n" + valorDaConta);
+    System.out.println("valor da gorgeta: %f\n" + valorDaGorgeta);
     System.out.println("porcentagem da gorgeta: %f\n" +DEZ_PORCENTO);
-    System.out.println("valor do total da gorgeta: %f\n" +valor_total);
+    System.out.println("valor do total da gorgeta: %f\n" +valorTotal);
     }
 }
