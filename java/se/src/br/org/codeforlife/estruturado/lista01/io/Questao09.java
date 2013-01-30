207cd848eb9f23ed69c1fc449b717ce3f3008ac5
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-30 10:52:15 -0300

    Modificado nomes das variaveis para o padrão

--- java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao09.java
+++ java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao09.java
@@ -14,46 +14,46 @@ public class Questao09 {
 
     public static void main(String args[]) {
         //declaração de variáveis
-        float primeira_nota = 0;
-        float primeiro_peso = 0;
+        float primeiraNota = 0;
+        float primeiroPeso = 0;
 
-        float segunda_nota = 0;
-        float segundo_peso = 0;
+        float segundaNota = 0;
+        float segundoPeso = 0;
 
-        float terceira_nota = 0;
-        float terceiro_peso = 0;
+        float terceiraNota = 0;
+        float terceiroPeso = 0;
 
-        float numerador_da_media_ponderada = 0;
-        float denominador_da_media_ponderada = 0;
+        float numeradorDaMediaPonderada = 0;
+        float denominadorDaMediaPonderada = 0;
         
          Scanner leia = new Scanner(System.in);
 
         //solicitação do pedido
         System.out.println("Informe a primeira nota: ");
-        primeira_nota = leia.nextFloat();
+        primeiraNota = leia.nextFloat();
         System.out.println("Informe o primeiro peso: ");
-        primeiro_peso = leia.nextFloat();
+        primeiroPeso = leia.nextFloat();
 
         System.out.println("Informe a segunda nota: ");
-        segunda_nota = leia.nextFloat();
+        segundaNota = leia.nextFloat();
         
         System.out.println("Informe o segundo peso: ");
-        segundo_peso = leia.nextFloat();
+        segundoPeso = leia.nextFloat();
 
         System.out.println("Informe a terceira nota: ");
-        terceira_nota = leia.nextFloat();
+        terceiraNota = leia.nextFloat();
         
         System.out.println("Informe o terceiro peso: ");
-        terceiro_peso = leia.nextFloat();
+        terceiroPeso = leia.nextFloat();
 
         //calculos
-        numerador_da_media_ponderada = primeira_nota * primeiro_peso + segunda_nota * segundo_peso + terceira_nota * terceiro_peso;
-        denominador_da_media_ponderada = primeiro_peso + segundo_peso + terceiro_peso;
+        numeradorDaMediaPonderada = primeiraNota * primeiroPeso + segundaNota * segundoPeso + terceiraNota * terceiroPeso;
+        denominadorDaMediaPonderada = primeiroPeso + segundoPeso + terceiroPeso;
         //demonstração do resultado
 
-        System.out.println("valor do numerador: \n" + numerador_da_media_ponderada);
-        System.out.println("valor do denominador: \n" + denominador_da_media_ponderada);
-        System.out.println("media do infeliz: \n"+ numerador_da_media_ponderada / denominador_da_media_ponderada);
+        System.out.println("valor do numerador: \n" + numeradorDaMediaPonderada);
+        System.out.println("valor do denominador: \n" + denominadorDaMediaPonderada);
+        System.out.println("media do infeliz: \n"+ numeradorDaMediaPonderada / denominadorDaMediaPonderada);
 
        
 
