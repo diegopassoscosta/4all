d5cb4135e791839759fb94247be2d94fc74d0aa1
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-30 10:52:20 -0300

    Modificado nomes das variaveis para o padrão

--- java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao10.java
+++ java/se/src/br/org/codeforlife/estruturado/lista01/io/Questao10.java
@@ -14,29 +14,29 @@ public class Questao10 {
 
     public static void main(String args[]) {
         //declaração de variáveis
-        float primeiro_termo = 0;
-        float segundo_termo = 0;
-        float terceiro_termo = 0;
+        float primeiroTermo = 0;
+        float segundoTermo = 0;
+        float terceiroTermo = 0;
         float razao = 0;
 
         Scanner leia = new Scanner(System.in);
         //solicitação do pedido
         System.out.println("Informe a primeira valor: ");
-        primeiro_termo = leia.nextInt();
+        primeiroTermo = leia.nextInt();
 
         System.out.println("Informe o segundo valor: ");
-        segundo_termo = leia.nextInt();
+        segundoTermo = leia.nextInt();
 
         //calculos
-        razao = segundo_termo - primeiro_termo;
-        terceiro_termo = segundo_termo + razao;
+        razao = segundoTermo - primeiroTermo;
+        terceiroTermo = segundoTermo + razao;
         //demonstração do resultado
 
-        System.out.println("primeiro termo: \n" + primeiro_termo);
-        System.out.println("segundo termo: \n" + segundo_termo);
+        System.out.println("primeiro termo: \n" + primeiroTermo);
+        System.out.println("segundo termo: \n" + segundoTermo);
         System.out.println("razao: \n" + razao);
-        System.out.println("terceiro termo: \n" + terceiro_termo);
-        System.out.println("progressao aritimetica \n" + primeiro_termo +" "+ segundo_termo +" "+ terceiro_termo);
+        System.out.println("terceiro termo: \n" + terceiroTermo);
+        System.out.println("progressao aritimetica \n" + primeiroTermo +" "+ segundoTermo +" "+ terceiroTermo);
 
     }
 }
