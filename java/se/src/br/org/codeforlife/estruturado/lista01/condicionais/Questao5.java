bd810c6713fc47d7e53a9dc9a9de1cd5ce4e2515
Diego Passos Costa <diegopassoscosta@gmail.com>
2013-01-30 10:27:54 -0300

    Modificado nomes das variaveis para o padrão

--- java/se/src/br/org/codeforlife/estruturado/lista01/condicionais/Questao5.java
+++ java/se/src/br/org/codeforlife/estruturado/lista01/condicionais/Questao5.java
@@ -13,30 +13,30 @@ import java.util.Scanner;
 public class Questao5 {
 
     public static void main(String args[]) {
-        int primeiro_termo = 0;
-        int segundo_termo = 0;
-        int proximo_termo = 0;
+        int primeiroTermo = 0;
+        int segundoTermo = 0;
+        int proximoTermo = 0;
         int razao = 0;
         
         Scanner leia = new Scanner(System.in);
 
         //solicitação do pedido
         System.out.println("Informe a primeira valor: ");
-        primeiro_termo = leia.nextInt();
+        primeiroTermo = leia.nextInt();
 
         System.out.println("Informe o segundo valor: ");
-        segundo_termo = leia.nextInt();
+        segundoTermo = leia.nextInt();
 
         //calculos
-        razao = segundo_termo - primeiro_termo;
+        razao = segundoTermo - primeiroTermo;
 
         //demonstração do resultado
-        System.out.println("%d\n" + primeiro_termo);
-        System.out.println("%d\n" + segundo_termo);
-        proximo_termo = segundo_termo + razao;
+        System.out.println("%d\n" + primeiroTermo);
+        System.out.println("%d\n" + segundoTermo);
+        proximoTermo = segundoTermo + razao;
         for (int i = 0; i < 10; i++) {
-            proximo_termo = proximo_termo + razao;
-            System.out.println("%d\n" + proximo_termo);
+            proximoTermo = proximoTermo + razao;
+            System.out.println("%d\n" + proximoTermo);
         }
     }
 }
