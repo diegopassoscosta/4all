adf5ed41317e3fb1e947867c3c5d525bf35521e9
Diego Passos Costa <diegopassoscosta@gmail.com>
2014-05-01 10:30:13 -0300

    renomeando arquivo e otimizando o if final

---
+++ ruby/is_palindrome_two_queues.rb
@@ -0,0 +1,33 @@
+require 'lineardslab.rb'
+include LinearDSLab
+include BitLab
+
+def is_palindrome(v)
+t = v.split(//)
+count = 0
+result = false
+
+q = Queue.new
+s = Queue.new
+
+##fazer tudo com um unico for
+for x in 0..t.length-1
+	q.enqueue(t[x])
+end
+
+(t.length-1).downto(0){ |x| s.enqueue(t[x])}
+
+max = q.count
+
+while q.count > 0
+  if q.dequeue == s.dequeue
+    count = count + 1
+  end
+end
+if count == max
+ result = true
+else
+ result = false
+end
+return result
+end
